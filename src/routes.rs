dd5bbc169dd1f7a2ef4b4baeb9e6c47af4741329
jardel <jardelcruz333@gmail.com>
2023-03-03 18:12:33 -0300

    Criando arquivos de rotas

---
+++ src/routes.rs
@@ -0,0 +1,14 @@
+use crate::services::user_services::{find_user, register_user};
+use axum::routing::{get, post};
+use axum::Router;
+
+pub fn routes() -> Router {
+    let user_routes = user_routes();
+    Router::new().nest("/user", user_routes)
+}
+
+fn user_routes() -> Router {
+    Router::new()
+        .route("/register", post(register_user))
+        .route("/find", get(find_user))
+}
